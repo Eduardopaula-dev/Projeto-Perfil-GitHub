522866ae7032df494b60e2f4f7c6c6ceb5842677
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-04 19:48:18 -0300

    refactor: implementar async/await e tratamento de erro na busca de usuário

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 
-const baseURL = 'https://api.github.com/user'
+const baseURL = 'https://api.github.com/users'
 
-btnSearch.addEventListener("click", () => {
+btnSearch.addEventListener("click", async () => {
     const userName = inputSearch.value;
     if(userName) {
-        const response = `${baseURL}/${userName}`
+        const response = await fetch(`${baseURL}/${userName}`)
 
-        if(!response) {
+        if(!response.ok) {
             alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente');
             return;
         }
 
-        const data = response.json();
+        const data = await response.json();
         console.log(data);
     } else {
         alert('Digite o nome do usuário')
