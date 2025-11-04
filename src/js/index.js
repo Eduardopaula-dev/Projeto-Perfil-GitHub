a7362a6754b132b7b31df65d387c083fdcf2dcb0
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-04 19:56:55 -0300

    refactor: adicionar tratamento de erros com try/catch na busca de usuário

--- src/js/index.js
+++ src/js/index.js
@@ -5,16 +5,24 @@ const baseURL = 'https://api.github.com/users'
 
 btnSearch.addEventListener("click", async () => {
     const userName = inputSearch.value;
-    if(userName) {
-        const response = await fetch(`${baseURL}/${userName}`)
 
-        if(!response.ok) {
-            alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente');
-            return;
+    if (userName) {
+
+        try {
+            const response = await fetch(`${baseURL}/${userName}`)
+
+            if (!response.ok) {
+                alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente');
+                return;
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Erro ao buscar o perfil de usuário:', error);
+            alert('Ocorreu um erro ao buscar o perfil do usuário. Por favor, tente mais tarde.')
+
         }
-
-        const data = await response.json();
-        console.log(data);
     } else {
         alert('Digite o nome do usuário')
     }
