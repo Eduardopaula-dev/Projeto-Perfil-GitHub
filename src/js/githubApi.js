e6ddc2388b60bfdbd80d7d6b32ca93b456b6a768
Eduardo de Paula <eduardodepaulatrabalho2@gmail.com>
2025-11-05 19:29:58 -0300

    feat: adicionar função para buscar repositórios do usuário no GitHub

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,8 +1,18 @@
+const baseURL = 'https://api.github.com/users';
+
 export async function fetchGithubUser(userName) {
-    const baseURL = 'https://api.github.com/users';
-    const response = await fetch(`${baseURL}/${userName}`);
-    if (!response.ok) {
-      throw new Error('Usuário não encontrado.');
-    }
-    return await response.json();
+  const response = await fetch(`${baseURL}/${userName}`);
+  if (!response.ok) {
+    throw new Error('Usuário não encontrado.');
   }
+  return await response.json();
+}
+
+export async function fetchGithubUserRepos(userName) {
+  const reponse = await fetch(`${baseURL}/${userName}/repos`)
+  if (!reponse.ok) {
+      throw new Error('Repositórios não encontrados.');
+  }
+  return await reponse.json();
+};
+  
